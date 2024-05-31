700d0c67e0ef3e6eb98aeeb60ae5cefadf545999
emmanuel <emmanuellawakin2005@gmail.com>
2024-05-31 21:20:23 +0100

    Fixed code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,31 +4,39 @@
 each game is in a prime number game"""
 
 
-def is_prime(num):
-    """This function checks if a number is prime"""
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-
 def isWinner(x, nums):
-    """This function determines who the winner of each game is"""
+    if x < 1 or not nums:
+        return None
+
+    max_n = max(nums)
+    
+    # Step 1: Create a list to determine prime numbers up to max_n using Sieve of Eratosthenes
+    is_prime = [True] * (max_n + 1)
+    is_prime[0], is_prime[1] = False, False
+    for start in range(2, int(max_n ** 0.5) + 1):
+        if is_prime[start]:
+            for multiple in range(start * start, max_n + 1, start):
+                is_prime[multiple] = False
+    
+    # Precompute the number of primes up to each number
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if is_prime[i] else 0)
+    
+    # Step 2: Determine the winner for each game
     maria_wins = 0
     ben_wins = 0
-
+    
     for n in nums:
-        primes_count = sum(1 for i in range(1, n + 1) if is_prime(i))
-        if primes_count % 2 == 0:
+        if prime_count[n] % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
-
+    
+    # Step 3: Determine the overall winner
     if maria_wins > ben_wins:
         return "Maria"
-    elif maria_wins < ben_wins:
+    elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
